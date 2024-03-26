973ed40de4f3daeb0196e8eff5e8762cdb6963ec
Chris Garate <gh33mann@gmail.com>
2024-03-26 14:40:54 -0400

    Figure out code for function of game strategy

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function getComputerChoice {
-    // insert function that will random return rock, paper, scissors //
+"use strict";
+
+let win = 'You Win!';
+let lose = 'You Lose!';
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+        if (choice = rock) {
+            return 'rock';
+        } else if (choice = paper){
+            return 'paper';
+        } else {
+            return 'scissors';
+        }
 }
 
+function playRound (playerSelection, computerSelection) {
+    //code here
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
